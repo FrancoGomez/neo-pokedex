78351e8f89c9815ad5719b20511453ad3035f31f
FrancoGomez <mgomez21699@gmail.com>
2022-08-25 16:29:03 -0300

    Delete innecesary variable

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -1,74 +1,56 @@
 const typePokemonColors = {
     bug: {
         backgroundColor: "#9bbb31",
-        color: "white",
     },
     dragon: {
         backgroundColor: "#006cc7",
-        color: "white",
     },
     fairy: {
         backgroundColor: "#FF70F5",
-        color: "white",
     },
     fire: {
         backgroundColor: "#FF850A",
-        color: "white",
     },
     ghost: {
         backgroundColor: "#9E2CB5",
-        color: "white",
     },
     ground: {
         backgroundColor: "#de7c4d",
-        color: "white",
     },
     normal: {
         backgroundColor: "#a0a29f",
-        color: "white",
     },
     psychic: {
         backgroundColor: "#fe8581",
-        color: "white",
     },
     steel: {
         backgroundColor: "#4f95a3",
-        color: "white",
     },
     dark: {
         backgroundColor: "#585761",
-        color: "white",
     },
     electric: {
         backgroundColor: "#FCD022",
-        color: "white",
     },
     fighting: {
         backgroundColor: "#d6435f",
-        color: "white",
     },
     flying: {
         backgroundColor: "#75A6E6",
-        color: "white",
     },
     grass: {
         backgroundColor: "#6abd5a",
-        color: "white",
     },
     ice: {
         backgroundColor: "#47C2AF",
-        color: "white",
     },
     poison: {
         backgroundColor: "#b066cf",
-        color: "white",
     },
     rock: {
         backgroundColor: "#AD9048",
-        color: "white",
     },
     water: {
         backgroundColor: "#349edf",
-        color: "white",
     },
 };
