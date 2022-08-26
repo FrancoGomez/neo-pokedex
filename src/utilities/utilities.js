79a558ac07c99e1fa349e5037dfeab2c0ee71286
FrancoGomez <mgomez21699@gmail.com>
2022-08-26 20:13:31 -0300

    Add pokemon generation because the api does not provide them the way is needed

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -955,3 +955,14 @@ const japanesePokemonNames = [
     "レイスポス",
     "バドレックス",
 ];
+
+const pokemonGenerations = [
+    "generation 1",
+    "generation 2",
+    "generation 3",
+    "generation 4",
+    "generation 5",
+    "generation 6",
+    "generation 7",
+    "generation 8",
+]
