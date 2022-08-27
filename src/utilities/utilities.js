914e41d1bbb9ea3e4436829d99329fc0c50235da
FrancoGomez <mgomez21699@gmail.com>
2022-08-27 15:21:51 -0300

    Change generations object and make it an object, so that the page can get how much pokemons per generation there are

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -956,13 +956,37 @@ const japanesePokemonNames = [
     "バドレックス",
 ];
 
-const pokemonGenerations = [
-    "generation 1",
-    "generation 2",
-    "generation 3",
-    "generation 4",
-    "generation 5",
-    "generation 6",
-    "generation 7",
-    "generation 8",
-]
+const pokemonPerGeneration = {
+    "generation 1": {
+        from: 1,
+        to: 151,
+    },
+    "generation 2": {
+        from: 152,
+        to: 251,
+    },
+    "generation 3": {
+        from: 252,
+        to: 386,
+    },
+    "generation 4": {
+        from: 387,
+        to: 493,
+    },
+    "generation 5": {
+        from: 494,
+        to: 649,
+    },
+    "generation 6": {
+        from: 650,
+        to: 721,
+    },
+    "generation 7": {
+        from: 722,
+        to: 809,
+    },
+    "generation 8": {
+        from: 810,
+        to: 861,
+    },
+};
